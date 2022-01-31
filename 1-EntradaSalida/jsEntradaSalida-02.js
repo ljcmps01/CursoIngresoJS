f2fdb3688edf810393e5d979ea8255ddf2ac42e5
ljcmps01 <ljcmps01@gmail.com>
2022-01-31 20:17:11 -0300

    Correccion de sintaxis

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -5,6 +5,7 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var dato =prompt("Ingresar dato"); 		//Salta el cartel pop up pidiendo una entrada
-	alert(dato);							//Salta el cartel mostrando la entrada del usuario
+	let nombreIngresado;
+	nombreIngresado=prompt("Ingresar nombre"); 		//Salta el cartel pop up pidiendo el nombre
+	alert("su nombre es "+nombreIngresado);			//Salta el cartel mostrando la entrada del usuario
 }
